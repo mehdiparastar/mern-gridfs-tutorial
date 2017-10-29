36706b382a6013ddd4f3d76415f3c45297e5dffd
AndreasLengkeek <andreas@propertybase.com>
2017-10-30 09:29:13 +1100

    Add list route for files

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log(`GridFS tutorial listening on ${port}`);
--- routes/file.js
+++ routes/file.js
@@ -7,13 +7,18 @@ var gfs = Grid(connection.db);
 
 // set up connection to db for file storage
 const storage = require('multer-gridfs-storage')({
-   db: connection.db
+   db: connection.db,
+   file: (req, file) => {
+      return {
+         filename: file.originalname
+      }
+   }
 });
 // sets file input to single file
 const singleUpload = multer({ storage: storage }).single('file');
 
 
-router.get('/file/:filename', (req, res) => {
+router.get('/files/:filename', (req, res) => {
    gfs.files.find({ filename: req.params.filename }).toArray((err, files) => {
       if(!files || files.length === 0){
          return res.status(404).json({
@@ -29,7 +34,23 @@ router.get('/file/:filename', (req, res) => {
    });
 });
 
-router.post('/file', singleUpload, (req, res) => {
+router.get('/files', (req, res) => {
+   gfs.files.find().toArray((err, files) => {
+      if(!files || files.length === 0){
+         return res.status(404).json({
+            message: "Could not find files"
+         });
+      }
+      return res.json(files);
+      var readstream = gfs.createReadStream({
+         filename: files[0].filename
+      })
+      res.set('Content-Type', files[0].contentType);
+      return readstream.pipe(res);
+   });
+});
+
+router.post('/files', singleUpload, (req, res) => {
    if (req.file) {
       return res.json({
          success: true,
